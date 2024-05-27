f4d0f3ee0a692467d0e257540a587e0143299f23
Ali <aghamolaeeali@gmail.com>
2024-05-27 15:00:09 +0330

    new line

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:paygah1/screens/welcome_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+// new line
 void main() {
   runApp(
     const MaterialApp(
